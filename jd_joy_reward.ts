4f42b6416c9a74201c998a388b1b01131736d819
小菜鸟爱学习 <523757325@qq.com>
2021-07-20 11:09:03 +0800

    joy reward

--- jd_joy_reward.ts
+++ jd_joy_reward.ts
@@ -1,16 +1,16 @@
 /**
- *
- * Running time limit:
- * s >= 58 or s <= 30  => exchange()
- * s > 30 and s < 58  =>  wait...
- * 
+ * 注意：
+ * 0～15秒才会进行兑换
+ * 16～59秒会进入死循环等待
  */
 import axios from 'axios';
-import USER_AGENT from './TS_USER_AGENTS';
+import {format} from 'date-fns';
+import USER_AGENT, {requireConfig, TotalBean, wait} from './TS_USER_AGENTS';
 import * as fs from 'fs';
 
-let $: any = {};
-let cookie: string = '', cookiesArr: Array<string> = [], validate: string = '';
+const notify = require('./sendNotify')
+
+let cookie: string = '', validate: string = '', UserName: string, index: number;
 let target: number = process.env.JD_JOY_REWARD_NAME ? parseInt(process.env.JD_JOY_REWARD_NAME) : 500;
 
 !(async () => {
@@ -19,16 +19,18 @@ let target: number = process.env.JD_JOY_REWARD_NAME ? parseInt(process.env.JD_JO
     validate_arr = validate_arr.split('\n')
     validate_arr.pop()
   }
-  await requireConfig();
+  let cookiesArr: any = await requireConfig();
 
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
-    $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
-    $.index = i + 1;
-    $.isLogin = true;
-    $.nickName = '';
-    await TotalBean();
-    console.log(`\n开始【京东账号${$.index}】${$.nickName || $.UserName}\n`);
+    UserName = decodeURIComponent(cookie.match(/pt_pin=([^;]*)/)![1])
+    index = i + 1;
+    let {isLogin, nickName}: any = await TotalBean(cookie)
+    if (!isLogin) {
+      notify.sendNotify(__filename.split('/').pop(), `cookie已失效\n京东账号${index}：${nickName || UserName}`)
+      continue
+    }
+    console.log(`\n开始【京东账号${index}】${nickName || UserName}\n`);
 
     if (i < validate_arr.length)
       validate = validate_arr[i]
@@ -73,16 +75,15 @@ function init() {
 }
 
 function exchange(beanId: number) {
-  console.log('exchange()')
   return new Promise<void>(async resolve => {
     while (1) {
-      let s: number = new Date().getSeconds();
-      if (s >= 58 || s <= 30) {
+      if (new Date().getSeconds() < 15) {
         break
       } else {
-        await wait(500)
+        await wait(100)
       }
     }
+    console.log('exchange()', format(new Date(), 'hh:mm:ss:SSS'))
     let {data} = await axios.post(`https://jdjoy.jd.com/common/gift/new/exchange?reqSource=h5&invokeKey=qRKHmL4sna8ZOP9F&validate=${validate}`,
       JSON.stringify({"buyParam": {"orderSource": 'pet', "saleInfoId": beanId}, "deviceInfo": {}}), {
         headers: {
@@ -96,57 +97,6 @@ function exchange(beanId: number) {
         }
       })
     console.log(data);
-    resolve();
-  })
-}
-
-function wait(t: number) {
-  return new Promise(e => setTimeout(e, t))
-}
-
-function requireConfig() {
-  return new Promise(resolve => {
-    console.log('\n====================Hello World====================\n');
-    console.log('开始获取配置文件\n');
-    const jdCookieNode = require('./jdCookie.js');
-    Object.keys(jdCookieNode).forEach((item) => {
-      if (jdCookieNode[item]) {
-        cookiesArr.push(jdCookieNode[item]);
-      }
-    })
-    console.log(`共${cookiesArr.length}个京东账号\n`);
-    resolve(0);
-  })
-}
-
-function TotalBean() {
-  return new Promise<void>(async resolve => {
-    axios.get('https://me-api.jd.com/user_new/info/GetJDUserInfoUnion', {
-      headers: {
-        Host: "me-api.jd.com",
-        Connection: "keep-alive",
-        Cookie: cookie,
-        "User-Agent": USER_AGENT,
-        "Accept-Language": "zh-cn",
-        "Referer": "https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&",
-        "Accept-Encoding": "gzip, deflate, br"
-      }
-    }).then(res => {
-      if (res.data) {
-        let data = res.data
-        if (data['retcode'] === "1001") {
-          $.isLogin = false; //cookie过期
-          return;
-        }
-        if (data['retcode'] === "0" && data['data'] && data.data.hasOwnProperty("userInfo")) {
-          $.nickName = data.data.userInfo.baseInfo.nickname;
-        }
-      } else {
-        console.log('京东服务器返回空数据');
-      }
-    }).catch(e => {
-      console.log('Error:', e)
-    })
     resolve();
   })
 }
